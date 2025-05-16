98c92480c2801aded8cab2092b006ba644c013c9
EduardoRuiiz <eduardoruiiz35@gmail.com>
2025-05-16 16:55:14 -0300

    Criação da solução ImportadorContratos com projeto WinForms

---
+++ ImportadorContratos/ImportadorContratos.app/TelaInicial.cs
@@ -0,0 +1,10 @@
+namespace ImportadorContratos.app
+{
+    public partial class TelaInicial : Form
+    {
+        public TelaInicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
